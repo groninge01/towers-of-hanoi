b3ee7a22883dde77e445ee4d5ddef76603120971
Stefan van Groningen <groninge@hotmail.com>
2018-02-02 20:38:20 +0100

    Added diskList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DiskListComponent } from './diskList.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/disk.ts
@@ -0,0 +1,3 @@
+export class Disk {
+    constructor(public id: number, public name: string) { }
+}
