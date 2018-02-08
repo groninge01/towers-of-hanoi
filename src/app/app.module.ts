f1dd9f5d3c161138240566f77fddaac1c0e53223
Stefan van Groningen <groninge@hotmail.com>
2018-02-08 20:10:53 +0100

    Re-ordered import list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DiskListComponent } from './disk-list/disk-list.component';
 
 import { AppComponent } from './app.component';
+import { DiskListComponent } from './disk-list/disk-list.component';
 import { TowerOfHanoiComponent } from './tower-of-hanoi/tower-of-hanoi.component';
 
 @NgModule({
