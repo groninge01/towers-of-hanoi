f9413370278b616eab376538721153a41387906d
Stefan van Groningen <groninge@hotmail.com>
2018-02-12 21:08:22 +0100

    Building the tower

--- src/app/tower-of-hanoi/tower-of-hanoi.component.ts
+++ src/app/tower-of-hanoi/tower-of-hanoi.component.ts
@@ -4,9 +4,11 @@ import { Disk } from '../disk-list/disk';
 @Component({
   selector: 'app-tower-of-hanoi',
   templateUrl: './tower-of-hanoi.component.html',
-  styleUrls: ['./tower-of-hanoi.component.css']
+  styleUrls: ['./tower-of-hanoi.component.scss']
 })
 export class TowerOfHanoiComponent implements OnInit {
+
+  Arr = Array; // Array type captured in a variable
 
   @Input() disk: Disk;
 
@@ -14,5 +16,6 @@ export class TowerOfHanoiComponent implements OnInit {
 
   ngOnInit() {
   }
+
 
 }
