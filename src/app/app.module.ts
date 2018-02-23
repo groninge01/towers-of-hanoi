dbbc7a527ac29ac2b8e66388ecebbf4533940e24
Stefan van Groningen <groninge@hotmail.com>
2018-02-23 20:02:59 +0100

    Added BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -14,6 +15,7 @@ import { TowerOfHanoiComponent } from './tower-of-hanoi/tower-of-hanoi.component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule
   ],
   providers: [],
