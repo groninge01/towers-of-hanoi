ccdac5f9a66afc39ac68a5fafe59a44c6ff6f189
Stefan van Groningen <groninge@hotmail.com>
2018-02-23 20:03:18 +0100

    Initial commit

---
+++ src/app/tower-of-hanoi/animations.ts
@@ -0,0 +1,6 @@
+import {animation, style, animate} from '@angular/animations';
+
+export const fadeAnimation = animation([
+  style({ opacity: '{{ from }}' }),
+  animate('{{ time }}', style({ opacity: '{{ to }}' }))
+], { params: { time: '1s' }});
