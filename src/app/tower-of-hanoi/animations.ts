84cb11506b88266893a103851df38492ab1cef3a
Stefan van Groningen <groninge@hotmail.com>
2018-02-25 15:23:37 +0100

    Removed params

--- src/app/tower-of-hanoi/animations.ts
+++ src/app/tower-of-hanoi/animations.ts
@@ -3,4 +3,4 @@ import {animation, style, animate} from '@angular/animations';
 export const fadeAnimation = animation([
   style({ opacity: '{{ from }}' }),
   animate('{{ time }}', style({ opacity: '{{ to }}' }))
-], { params: { time: '1s' }});
+]);
